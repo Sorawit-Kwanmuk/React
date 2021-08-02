3e408fdf05f62905a54cd5f6acaa913fdb56fcd4
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-02 11:31:55 +0700

    add lab4.3

---
+++ lab4.3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div className='counter'>
+      <button className='btn'>+</button>
+      <span className='current'>0</span>
+      <button className='btn'>-</button>
+      <button className='btn'>Reset</button>
+    </div>
+  );
+}
+
+export default App;
